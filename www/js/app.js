82bf3bc29a35ae6663e8a76eeb9fe84f0f6ff477
Ian Campelo <iancampelo@gmail.com>
2015-10-27 17:49:46 -0200

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,36 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var myApp = angular.module('starter', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+
+myApp.controller("ExampleController", ['$scope',function($scope, $cordovaMedia, $ionicLoading) {
+
+  $scope.play = function(src) {
+    var media = new Media(src, null, null, mediaStatusCallback);
+    $cordovaMedia.play(media);
+  }
+
+  var mediaStatusCallback = function(status) {
+    if(status == Media.MEDIA_STARTING) {
+      $ionicLoading.show({template: 'Loading...'});
+    } else {
+      $ionicLoading.hide();
+    }
+  }
+}]);
